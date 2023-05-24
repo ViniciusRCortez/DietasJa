92b5c9370433acd1eeb7fba4f517d5c4f477933f
Vinicius Cortêz <vinisicius2@gmail.com>
2023-05-23 21:23:38 -0300

    [#1]: Gerando Swagger e Redoc para a documentação automatica da API

--- BackEnd/api/api/settings.py
+++ BackEnd/api/api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'drf_yasg',
     'Users'
 ]
 
--- BackEnd/api/api/urls.py
+++ BackEnd/api/api/urls.py
@@ -19,9 +19,27 @@ from django.urls import path
 from Users.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DietasJá API",
+        default_version="v1",
+        description="API de app de contagem de calorias",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
