a4c3040a0cd33165f787e631ac755aeef652735f
= <=>
2023-06-17 10:24:55 -0300

    [#8]: Adicionei o campo porcao ao Alimento

--- BackEnd/api/Alimentos/serializers.py
+++ BackEnd/api/Alimentos/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class AlimentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alimento
-        fields = ['id', 'nome', 'qtd_calorias', 'qtd_carboidratos', 'qtd_gorduras', 'qtd_proteinas', 'e_padrao']
+        fields = ['id', 'nome', 'porcao', 'qtd_calorias', 'qtd_carboidratos', 'qtd_gorduras', 'qtd_proteinas', 'e_padrao']
