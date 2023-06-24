a6975c96e4723b572c8bfea7cf81731da4fd3430
Luiz-021 <luizgute@outlook.com>
2023-06-23 23:55:40 -0300

    [2#] Correções

--- BackEnd/api/Consumo/models.py
+++ BackEnd/api/Consumo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from MetaDiaria.models import MetaDiaria
 from Refeicao.models import Refeicao
+from django.db.models import Sum
 
 
 class Consumo(models.Model):
@@ -18,9 +19,9 @@ class Consumo(models.Model):
 
     def atualizar_valores_acumulados(self):
         # calcular os valores acumulados
-        self.calorias_total = self.refeicoes_list.aggregate(Sum('calorias_total'))['calorias_total__sum'] or 0
-        self.carboidratos_total = self.refeicoes_list.aggregate(Sum('carboidratos_total'))['carboidratos_total__sum'] or 0
-        self.porcao = self.refeicoes_list.aggregate(Sum('porcao'))['porcao__sum'] or 0
-        self.gorduras_total = self.refeicoes_list.aggregate(Sum('gorduras_total'))['gorduras_total__sum'] or 0
-        self.proteinas_total = self.refeicoes_list.aggregate(Sum('proteinas_total'))['proteinas_total__sum'] or 0
+        self.calorias_total = self.consumidos_list.aggregate(Sum('calorias_total'))['calorias_total__sum'] or 0
+        self.carboidratos_total = self.consumidos_list.aggregate(Sum('carboidratos_total'))['carboidratos_total__sum'] or 0
+        self.porcao = self.consumidos_list.aggregate(Sum('porcao'))['porcao__sum'] or 0
+        self.gorduras_total = self.consumidos_list.aggregate(Sum('gorduras_total'))['gorduras_total__sum'] or 0
+        self.proteinas_total = self.consumidos_list.aggregate(Sum('proteinas_total'))['proteinas_total__sum'] or 0
 
--- BackEnd/api/Refeicao/models.py
+++ BackEnd/api/Refeicao/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Alimentos.models import Alimento
+from django.db.models import Sum
 
 class Refeicao(models.Model):
     nome = models.CharField(max_length = 100)
@@ -13,11 +14,11 @@ class Refeicao(models.Model):
 
     def atualizar_campos_acumulados(self):
         # Atualiza os campos de informações nutricionais da refeição antes de salvar
-        self.calorias = self.alimentos_list.aggregate(Sum('qtd_calorias'))['calorias__sum'] or 0
+        self.calorias_total = self.alimentos_list.aggregate(Sum('qtd_calorias'))['qtd_calorias__sum'] or 0
         self.porcao = self.refeicoes_list.aggregate(Sum('porcao'))['porcao__sum'] or 0
-        self.carboidratos = self.alimentos_list.aggregate(Sum('qtd_carboidratos'))['carboidratos__sum'] or 0
-        self.gorduras = self.alimentos_list.aggregate(Sum('qtd_gorduras'))['gorduras__sum'] or 0
-        self.proteinas = self.alimentos_list.aggregate(Sum('qtd_proteinas'))['proteinas__sum'] or 0
+        self.carboidratos_total = self.alimentos_list.aggregate(Sum('qtd_carboidratos'))['qtd_carboidratos__sum'] or 0
+        self.gorduras_total = self.alimentos_list.aggregate(Sum('qtd_gorduras'))['qtd_gorduras__sum'] or 0
+        self.proteinas_total = self.alimentos_list.aggregate(Sum('qtd_proteinas'))['qtd_proteinas__sum'] or 0
     
     def __str__ (self):
         return self.nome
