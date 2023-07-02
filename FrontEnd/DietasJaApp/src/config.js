e88a831399cf660e6c28cbd0202db59c11427ca2
Vinicius Cortêz <vinisicius2@gmail.com>
2023-07-02 17:38:13 -0300

    Ajuste para consumo de externo

--- FrontEnd/DietasJaApp/src/config.js
+++ FrontEnd/DietasJaApp/src/config.js
@@ -1,4 +1,5 @@
 //ATENÇÃO - Devido a problemas de conexão do expo com o localhost, essa variavel aqui não poderá ser 
 //          localhost mas, ou devemos utilizar de uma cloud pra rodar o back ou usar o ngrok, de ambas 
 //          maneiras atentem-se a o valor dessa variavel, pq ela pode mudar
+
 export const API_BASE_URL = ' https://bedc-2804-29b8-516a-970-b0dc-69ee-92f7-7b89.ngrok-free.app' + '/api';
