2c97dcfb6f1b586b757b09d5b961b5be67fd31da
Vinicius Cortêz <vinisicius2@gmail.com>
2023-07-02 17:39:25 -0300

    Ajuste para consumo de externo 2

--- FrontEnd/DietasJaApp/src/config.js
+++ FrontEnd/DietasJaApp/src/config.js
@@ -2,4 +2,5 @@
 //          localhost mas, ou devemos utilizar de uma cloud pra rodar o back ou usar o ngrok, de ambas 
 //          maneiras atentem-se a o valor dessa variavel, pq ela pode mudar
 
-export const API_BASE_URL = ' https://bedc-2804-29b8-516a-970-b0dc-69ee-92f7-7b89.ngrok-free.app' + '/api';
+export const API_BASE_URL = 'https://dietasja-back-production.up.railway.app' + '/api';
+
